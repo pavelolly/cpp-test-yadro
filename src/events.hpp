c22af19d644de307e8d3d455e8cc3b6fcbe8e65a
km <lvanfomin069@gmail.com>
2025-05-01 15:34:21 +0300

    update events.hpp

--- src/events.hpp
+++ src/events.hpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <string>
 #include <string_view>
+#include <concepts>
 
 namespace event {
 
@@ -41,13 +42,29 @@ template<> struct BodyFor<EventId::IN_CLIENT_START>  { using type = BodyClientTa
 template<> struct BodyFor<EventId::OUT_CLIENT_START> { using type = BodyClientTable; };
 template<> struct BodyFor<EventId::OUT_ERROR>        { using type = BodyError; };
 
+template <EventId Id>
+using BodyForType = typename BodyFor<Id>::type;
+
 class Event {
 
-    template <EventId Id, typename BodyType = typename BodyFor<Id>::type>
-    static Event Create(std::tm time, BodyType body) {
-        return Event(id, time, std::make_unique<BodyType>(std::move(body)));
+    template <EventId Id>
+    static Event Create(std::tm time, BodyForType<Id> body) {
+        return Event(Id, time, std::make_unique<BodyForType<Id>>(std::move(body)));
     }
 
+    EventId GetId()   const { return id_; }
+    std::tm GetTime() const { return time_; }
+    EventBody &GetBody()    { return *body_; }
+    const EventBody &GetBody() const { return *body_; }
+
+    template <typename BodyType> requires std::convertible_to<BodyType, EventBody>
+    BodyType &GetBody() { return static_cast<BodyType &>(*body_); }
+
+    template <typename BodyType> requires std::convertible_to<BodyType, EventBody>
+    const BodyType &GetBody() const { return static_cast<const BodyType &>(*body_); }
+
+    bool HasBody() const { return static_cast<bool>(body_); }
+    
 private:
     Event(EventId id, std::tm time, std::unique_ptr<EventBody> body = nullptr)
         : id_(id), time_(time), body_(std::move(body)) {}
