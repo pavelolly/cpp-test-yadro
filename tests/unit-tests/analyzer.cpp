7d80a6102c8fd84fe5a805b16a81a379093406fb
km <lvanfomin069@gmail.com>
2025-05-05 15:22:42 +0300

    duplicate some tests to unit-tests

--- tests/unit-tests/analyzer.cpp
+++ tests/unit-tests/analyzer.cpp
@@ -69,6 +69,125 @@ TEST(ProcessInputData, Events) {
        "19:00 11 client3\n"
     );
 
+    LoadAndCompare(
+        "10\n"
+        "08:00 22:00\n"
+        "100\n"
+        "07:00 2 kasumi 10\n"
+        "07:45 1 liara\n"
+        "08:15 1 jeff\n"
+        "08:55 4 kelly\n"
+        "09:38 2 samantha 5\n"
+        "10:10 2 miranda 8\n"
+        "10:48 4 wrex\n"
+        "11:23 3 garrus\n"
+        "12:00 1 garrus\n"
+        "12:31 4 edi\n"
+        "13:05 1 samantha\n"
+        "13:47 2 edi 4\n"
+        "14:24 3 liara\n"
+        "14:58 4 jeff\n"
+        "15:43 3 miranda\n"
+        "16:21 3 kasumi\n"
+        "16:52 2 thane 4\n"
+        "17:26 4 kelly\n"
+        "18:12 2 edi 6\n"
+        "18:54 1 liara\n"
+        "19:38 1 kelly\n"
+        "20:11 1 miranda\n"
+        "20:49 1 ashley\n"
+        "21:23 4 garrus\n"
+        "22:07 4 miranda\n"
+        ,
+        "07:00 2 kasumi 10\n"
+        "07:00 13 ClientUnknown\n"
+        "07:45 1 liara\n"
+        "07:45 13 NotOpenYet\n"
+        "08:15 1 jeff\n"
+        "08:55 4 kelly\n"
+        "08:55 13 ClientUnknown\n"
+        "09:38 2 samantha 5\n"
+        "09:38 13 ClientUnknown\n"
+        "10:10 2 miranda 8\n"
+        "10:10 13 ClientUnknown\n"
+        "10:48 4 wrex\n"
+        "10:48 13 ClientUnknown\n"
+        "11:23 3 garrus\n"
+        "11:23 13 ClientUnknown\n"
+        "12:00 1 garrus\n"
+        "12:31 4 edi\n"
+        "12:31 13 ClientUnknown\n"
+        "13:05 1 samantha\n"
+        "13:47 2 edi 4\n"
+        "13:47 13 ClientUnknown\n"
+        "14:24 3 liara\n"
+        "14:24 13 ClientUnknown\n"
+        "14:58 4 jeff\n"
+        "15:43 3 miranda\n"
+        "15:43 13 ClientUnknown\n"
+        "16:21 3 kasumi\n"
+        "16:21 13 ClientUnknown\n"
+        "16:52 2 thane 4\n"
+        "16:52 13 ClientUnknown\n"
+        "17:26 4 kelly\n"
+        "17:26 13 ClientUnknown\n"
+        "18:12 2 edi 6\n"
+        "18:12 13 ClientUnknown\n"
+        "18:54 1 liara\n"
+        "19:38 1 kelly\n"
+        "20:11 1 miranda\n"
+        "20:49 1 ashley\n"
+        "21:23 4 garrus\n"
+        "22:07 4 miranda\n"
+        "22:00 11 ashley\n"
+        "22:00 11 kelly\n"
+        "22:00 11 liara\n"
+        "22:00 11 samantha\n"
+    );
+
+    LoadAndCompare(
+        "5\n"
+        "08:00 22:00\n"
+        "100\n"
+        "07:00 1 thane\n"
+        "08:19 1 wrex\n"
+        "09:34 3 wrex\n"
+        "10:30 1 kaidan\n"
+        "11:25 1 garrus\n"
+        "12:21 4 kaidan\n"
+        "13:21 2 garrus 3\n"
+        "14:09 2 garrus 3\n"
+        "15:01 2 wrex 4\n"
+        "16:19 4 wrex\n"
+        "17:35 2 garrus 5\n"
+        "18:41 3 garrus\n"
+        "20:00 1 ashley\n"
+        "21:14 2 garrus 5\n"
+        "22:13 1 shepard\n"
+        ,
+        "07:00 1 thane\n"
+        "07:00 13 NotOpenYet\n"
+        "08:19 1 wrex\n"
+        "09:34 3 wrex\n"
+        "09:34 13 ICanWaitNoLonger!\n"
+        "10:30 1 kaidan\n"
+        "11:25 1 garrus\n"
+        "12:21 4 kaidan\n"
+        "13:21 2 garrus 3\n"
+        "14:09 2 garrus 3\n"
+        "15:01 2 wrex 4\n"
+        "16:19 4 wrex\n"
+        "17:35 2 garrus 5\n"
+        "18:41 3 garrus\n"
+        "18:41 13 ICanWaitNoLonger!\n"
+        "20:00 1 ashley\n"
+        "21:14 2 garrus 5\n"
+        "22:13 1 shepard\n"
+        "22:13 13 NotOpenYet\n"
+        "22:00 11 ashley\n"
+        "22:00 11 garrus\n"
+    );
+
 #undef LoadAndCompare
 }
 
@@ -113,6 +232,81 @@ TEST(ProcessInputData, TableInfos) {
        "12:43 4 client2\n"
        "15:52 4 client4\n",
        map
+    );
+
+    map = {
+        {1, {0, TimeStamp()}},
+        {2, {0, TimeStamp()}},
+        {3, {0, TimeStamp()}},
+        {4, {0, TimeStamp()}},
+        {5, {0, TimeStamp()}},
+        {6, {0, TimeStamp()}},
+        {7, {0, TimeStamp()}},
+        {8, {0, TimeStamp()}},
+        {9, {0, TimeStamp()}},
+        {10, {0, TimeStamp()}},
+    };
+
+    LoadAndCompare(
+        "10\n"
+        "08:00 22:00\n"
+        "100\n"
+        "07:00 2 kasumi 10\n"
+        "07:45 1 liara\n"
+        "08:15 1 jeff\n"
+        "08:55 4 kelly\n"
+        "09:38 2 samantha 5\n"
+        "10:10 2 miranda 8\n"
+        "10:48 4 wrex\n"
+        "11:23 3 garrus\n"
+        "12:00 1 garrus\n"
+        "12:31 4 edi\n"
+        "13:05 1 samantha\n"
+        "13:47 2 edi 4\n"
+        "14:24 3 liara\n"
+        "14:58 4 jeff\n"
+        "15:43 3 miranda\n"
+        "16:21 3 kasumi\n"
+        "16:52 2 thane 4\n"
+        "17:26 4 kelly\n"
+        "18:12 2 edi 6\n"
+        "18:54 1 liara\n"
+        "19:38 1 kelly\n"
+        "20:11 1 miranda\n"
+        "20:49 1 ashley\n"
+        "21:23 4 garrus\n"
+        "22:07 4 miranda\n",
+        map
+    );
+
+    map = {
+        {1, {0, TimeStamp()}},
+        {2, {0, TimeStamp()}},
+        {3, {500, TimeStamp(4, 14)}},
+        {4, {200, TimeStamp(1, 18)}},
+        {5, {500, TimeStamp(4, 25)}},
+    };
+
+    LoadAndCompare(
+        "5\n"
+        "08:00 22:00\n"
+        "100\n"
+        "07:00 1 thane\n"
+        "08:19 1 wrex\n"
+        "09:34 3 wrex\n"
+        "10:30 1 kaidan\n"
+        "11:25 1 garrus\n"
+        "12:21 4 kaidan\n"
+        "13:21 2 garrus 3\n"
+        "14:09 2 garrus 3\n"
+        "15:01 2 wrex 4\n"
+        "16:19 4 wrex\n"
+        "17:35 2 garrus 5\n"
+        "18:41 3 garrus\n"
+        "20:00 1 ashley\n"
+        "21:14 2 garrus 5\n"
+        "22:13 1 shepard\n",
+        map
     );
 }
 
